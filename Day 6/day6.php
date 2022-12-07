88ba86be083f1caa006e3b38799c6083028ea55f
Christophe SAUVEUR <chsxf.pro@gmail.com>
2022-12-07 07:22:37 +0100

    🗓 Day 6

---
+++ Day 6/day6.php
@@ -0,0 +1,25 @@
+<?php
+require_once('../common.inc.php');
+$input = loadInput(__FILE__);
+
+define('CHAR_COUNT', 4);
+
+$result = 0;
+$buffer = [];
+for ($i = 0; $i < strlen($input); $i++) {
+    if (count($buffer) == CHAR_COUNT) {
+        array_shift($buffer);
+    }
+
+    if (count($buffer) < CHAR_COUNT) {
+        $buffer[] = $input[$i];
+    }
+
+    $uniqueValues = array_unique($buffer);
+    if (count($uniqueValues) == CHAR_COUNT) {
+        $result = $i + 1;
+        break;
+    }
+}
+
+var_dump($result);
--- common.inc.php
+++ common.inc.php
@@ -1,8 +1,18 @@
 <?php
 define('USE_TEST_INPUT', count($argv) > 1 && $argv[1] == '-test');
 
-function loadLines($sourceFileName): array {
+function computeInputPath($sourceFileName): string {
 	$basePath = preg_replace('/(?:-\d)?\.php$/', '', $sourceFileName);
 	$inputPath = $basePath . (USE_TEST_INPUT ? '.testInput' : '.input') . '.txt';
+	return $inputPath;
+}
+
+function loadLines($sourceFileName): array {
+	$inputPath = computeInputPath($sourceFileName);
 	return file($inputPath, FILE_IGNORE_NEW_LINES);
+}
+
+function loadInput($sourceFileName): string {
+	$inputPath = computeInputPath($sourceFileName);
+	return file_get_contents($inputPath);
 }
